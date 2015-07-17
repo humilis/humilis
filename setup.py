e4e638aae0c366234f662b886f26cdc1b6badce1
German Gomez-Herrero <g@germangh.com>
2015-07-17 18:25:50 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name="humilis",
+    version="0.3a0",
+    author="German Gomez-Herrero",
+    author_email='g@germangh.com',
+    license='Apache Software License 2.0',
+    description='Manages AWS Cloudformation stacks',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'PyYAML',
+        'click',
+        'boto',
+        'jinja2'],
+    entry_points={
+        'console_scripts': [
+            'humilis=humilis:cli']}
+)
