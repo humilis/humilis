61f8c7bab88b60f84f0a78041093db3da0bd4226
German Gomez-Herrero <g@germangh.com>
2015-07-20 01:00:18 +0200

    Create sample environment scheleton, reorganize CLI

--- humilis/cli.py
+++ humilis/cli.py
@@ -13,15 +13,25 @@ def validate_log_level(ctx, param, value):
         raise click.BadParameter('Should one of %s' % LOG_LEVELS)
 
 
-@click.command()
+@click.group()
 @click.option('--log', default='info', help='Log level: CRITICAL, ERROR, '
               'WARNING, INFO or DEBUG', callback=validate_log_level)
 @click.option('--botolog', default='info', help='Boto log level: CRITICAL, '
               'ERROR, WARNING, INFO or DEBUG', callback=validate_log_level)
+def main(log, botolog):
+    print("log={}, botolog={}".format(log, botolog))
+
+
+@main.command()
 @click.argument('environment')
-def main(environment, log, botolog):
-    print("environment={}, log={}, botolog={}".format(environment, log,
-                                                      botolog))
+def create(environment):
+    click.echo("You have called the create action")
+
+
+@main.command()
+@click.argument('environment')
+def delete(environment):
+    click.echo("You have called the delete action")
 
 
 if __name__ == '__main__':
