1718b186844567c681dac4279a6c7abaf7fb1e64
German Gomez-Herrero <g@germangh.com>
2015-07-20 01:08:58 +0200

    Update tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,14 +7,26 @@ import humilis.cli
 import pytest
 
 
+ENV_ACTIONS = ['create', 'delete']
+LOG_LEVELS = ['critical', 'error', 'warning', 'info', 'debug']
+
+
 @pytest.yield_fixture(scope="module")
 def runner():
     yield CliRunner()
 
 
-def test_basic(runner):
-    result = runner.invoke(humilis.cli.main, ['testenv'])
-    assert result.exit_code == 0
+def test_actions(runner):
+    for action in ENV_ACTIONS:
+        result = runner.invoke(humilis.cli.main, [action, 'testenv'])
+        assert result.exit_code == 0
+
+
+def test_actions_with_missing_environment(runner):
+    for action in ENV_ACTIONS:
+        result = runner.invoke(humilis.cli.main, [action])
+        assert result.exit_code > 0
+        assert isinstance(result.exception, SystemExit)
 
 
 def test_invalid_log_level(runner):
@@ -25,17 +37,20 @@ def test_invalid_log_level(runner):
 
 def test_valid_log_level(runner):
     for level in ['critical', 'error', 'warning', 'info', 'debug']:
-        result = runner.invoke(humilis.cli.main, ['testenv', '--log', level])
+        result = runner.invoke(humilis.cli.main, ['--log', level, 'create',
+                                                  'testenv'])
         assert result.exit_code == 0
 
 
 def test_invalid_botolog_level(runner):
-    result = runner.invoke(humilis.cli.main, ['--log', 'invalid'])
+    result = runner.invoke(humilis.cli.main, ['--log', 'invalid', 'create',
+                                              'testenv'])
     assert result.exit_code > 0
     assert isinstance(result.exception, SystemExit)
 
 
 def test_valid_botolog_level(runner):
-    for level in ['critical', 'error', 'warning', 'info', 'debug']:
-        result = runner.invoke(humilis.cli.main, ['testenv', '--log', level])
+    for level in LOG_LEVELS:
+        result = runner.invoke(humilis.cli.main, ['--log', level, 'create',
+                                                  'testenv'])
         assert result.exit_code == 0
