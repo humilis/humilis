42a54407fc1009b3bd46a3ff313cede388c1265b
German Gomez-Herrero <g@germangh.com>
2016-01-06 11:22:13 +0100

    Upgrade boto3facade to 0.0.4

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         'PyYAML',
         'Click',
         'boto3',
-        'boto3facade==0.0.3',
+        'boto3facade==0.0.4',
         'jinja2'],
     classifiers=[
         "Programming Language :: Python :: 3"],
