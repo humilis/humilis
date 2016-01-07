03dadd55d453d5314702626e87ece0fd5d95601d
German Gomez-Herrero <g@germangh.com>
2016-01-07 11:44:06 +0100

    Better packaging

---
+++ humilis/metadata.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""Project metadata."""
+
+package = 'humilis'
+project = 'humilis'
+version = '0.0.1'
+description = 'Helps you deploy infrastructure in the AWS cloud'
+authors = ['German Gomez-Herrero', 'Innovative Travel Ltd']
+authors_string = ', '.join(authors)
+emails = ['german@innovativetravel.eu']
+license = 'MIT'
+copyright = '2015 ' + authors_string
+url = 'http://github.com/InnovativeTravel/humilis'
--- setup.py
+++ setup.py
@@ -2,15 +2,22 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
+import boto3facade.metadata as metadata
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
 
 setup(
-    name="humilis",
-    version="0.4",
-    author="Innovative Travel Ltd.",
-    author_email='german@innovativetravel.eu',
-    url='http://github.com/germangh/humilis',
-    license='Apache Software License 2.0',
-    description='Helps you deploy AWS CloudFormation stacks',
+    name=metadata.project,
+    version=metadata.version,
+    author=metadata.authors[0],
+    author_email=metadata.emails[0],
+    url=metadata.url,
+    license=metadata.license,
+    description=metadata.description,
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -21,6 +28,13 @@ setup(
         'jinja2'],
     classifiers=[
         "Programming Language :: Python :: 3"],
+    # Allow tests to be run with `python setup.py test'.
+    tests_require=[
+        'pytest>=2.5.1',
+        'mock>=1.0.1',
+        'flake8>=2.1.0'
+    ],
+    zip_safe=False,
     entry_points={
         'console_scripts': [
             'humilis=humilis.cli:main']}
