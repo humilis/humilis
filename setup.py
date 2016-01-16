66ab79c3ecedb471f841980d68fd1c9ab0de1451
German Gomez-Herrero <g@germangh.com>
2016-01-16 17:27:30 +0100

    Use package_data in setup.py to include examples in the installed package

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
     license=metadata.license,
     description=metadata.description,
     packages=find_packages(),
+    package_data = {'': ['*.yaml', '*.ini', '*.yml', '*.zip', '*.json']},
     include_package_data=True,
     install_requires=[
         'PyYAML',
