0779d792b7e2dc9de6a11b44c33558ba959f5cf8
German Gomez-Herrero <g@germangh.com>
2016-01-16 20:43:43 +0100

    Manage properly stage option in CLI

--- humilis/cli.py
+++ humilis/cli.py
@@ -19,7 +19,7 @@ def validate_log_level(ctx, param, value):
 
 
 @click.group()
-@click.option('--log', default='info', help="Log level :{}".format(LOG_LEVELS),
+@click.option('--log', default='info', help="Log level: {}".format(LOG_LEVELS),
               callback=validate_log_level, metavar='LEVEL')
 @click.option('--profile', default='default', metavar='NAME')
 def main(log, profile):
@@ -29,13 +29,14 @@ def main(log, profile):
 
 @main.command()
 @click.argument('environment')
-@click.option('--stage', help="Deployment stage, e.g. PRODUCTION, or DEV")
+@click.option('--stage', help="Deployment stage, e.g. PRODUCTION, or DEV",
+              default=None, metavar='STAGE')
 @click.option('--output', help="Store environment outputs in a yaml file",
               default=None, metavar='FILE')
 @click.option('--pretend/--no-pretend', default=False)
 def create(environment, stage, output, pretend):
     """Creates an environment."""
-    env = Environment(environment)
+    env = Environment(environment, stage=stage)
     if not pretend:
         env.create(output_file=output)
 
@@ -43,11 +44,11 @@ def create(environment, stage, output, pretend):
 @main.command()
 @click.argument('environment')
 @click.option('--stage', help="Deployment stage, e.g. PRODUCTION, or DEV",
-              default='')
+              default=None, metavar='STAGE')
 @click.option('--pretend/--no-pretend', default=False)
 def delete(environment, stage, pretend):
     """Deletes an environment that has been deployed to CF."""
-    env = Environment(environment)
+    env = Environment(environment, stage=stage)
     if not pretend:
         env.delete()
 
