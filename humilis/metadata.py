2165c97259458de1c1aa4fd6a60e8ec28a844adc
German Gomez-Herrero <g@germangh.com>
2016-02-06 23:27:05 +0100

    Release 0.1.5: jinja2 support for lambdas

--- humilis/metadata.py
+++ humilis/metadata.py
@@ -3,7 +3,7 @@
 
 package = 'humilis'
 project = 'humilis'
-version = '0.1.4'
+version = '0.1.5'
 description = 'Helps you deploy infrastructure in the AWS cloud'
 authors = ['German Gomez-Herrero', 'Innovative Travel Ltd']
 authors_string = ', '.join(authors)
