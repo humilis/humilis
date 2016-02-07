5611f5461a43d103b54fcf3fbe5c0ffb3b5a5066
German Gomez-Herrero <g@germangh.com>
2016-02-07 21:09:09 +0100

    Release 0.1.9: bugfix

--- humilis/metadata.py
+++ humilis/metadata.py
@@ -3,7 +3,7 @@
 
 package = 'humilis'
 project = 'humilis'
-version = '0.1.8'
+version = '0.1.9'
 description = 'Helps you deploy infrastructure in the AWS cloud'
 authors = ['German Gomez-Herrero', 'Innovative Travel Ltd']
 authors_string = ', '.join(authors)
