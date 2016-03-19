44f5f5294b7df83aff397ccb6041ffb249deba2b
German Gomez-Herrero <german@innovativetravel.eu>
2016-03-19 16:42:15 +0100

    Release 0.3.6

--- humilis/metadata.py
+++ humilis/metadata.py
@@ -3,7 +3,7 @@
 
 package = 'humilis'
 project = 'humilis'
-version = '0.3.5'
+version = '0.3.6'
 description = 'Helps you deploy infrastructure in the AWS cloud'
 authors = ['Innovative Travel Ltd', 'German Gomez-Herrero']
 authors_string = ', '.join(authors)
