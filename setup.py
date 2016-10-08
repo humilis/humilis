5403905ef5a8edc462e26ab951dc75351e04abe7
German Gomez-Herrero <german@innovativetravel.eu>
2016-10-08 23:12:53 +0200

    Upgrade boto3facade

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         "Click",
         "boto3",
         "s3keyring",
-        "boto3facade>=0.3.9",
+        "boto3facade>=0.4.0",
         "jinja2"],
     classifiers=[
         "Programming Language :: Python :: 2",
