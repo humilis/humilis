e6d17344c49dd4356cd51ee3ea9cfad404a80f4b
German Gomez-Herrero <german@innovativetravel.eu>
2017-04-05 20:59:39 +0200

    BUG: don't break if a layer does not have parameters

--- humilis/layer.py
+++ humilis/layer.py
@@ -114,8 +114,8 @@ class Layer:
     @property
     def termination_protection(self):
         """Is termination protection set for this layer?."""
-        return self.meta['parameters'].get('termination_protection', {}).get(
-            'value', False)
+        return self.meta.get('parameters', {}).get(
+            'termination_protection', {}).get('value', False)
 
     @property
     def cf_name(self):
