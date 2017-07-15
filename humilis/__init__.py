8ad7ca18ddfa7f14c2eacad562aea7ae022ea282
German Gomez-Herrero <german@innovativetravel.eu>
2017-07-15 14:21:30 +0200

    Release

--- humilis/__init__.py
+++ humilis/__init__.py
@@ -1,8 +1,8 @@
-"""AWS Cloudformation-based deployment framework."""
+"""AWS Cloudformation-based deployment helper."""
 
 import os
 import inspect
 
 
-__version__ = "1.4.6"
+__version__ = "1.4.7"
 __dir__ = os.path.dirname(inspect.getfile(inspect.currentframe()))
