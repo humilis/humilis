82236818da5bfd04ef96a9c7e8c67606b37552f7
German Gomez-Herrero <german@findhotel.net>
2022-10-15 21:29:38 +0200

    Fix deprecated pypandoc method

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ dirname = os.path.dirname(__file__)
 
 try:
     import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
+    long_description = pypandoc.convert_file('README.md', 'rst')
 except(IOError, ImportError, RuntimeError):
     if os.path.isfile("README.md"):
         long_description = codecs.open(os.path.join(dirname, "README.md"),
